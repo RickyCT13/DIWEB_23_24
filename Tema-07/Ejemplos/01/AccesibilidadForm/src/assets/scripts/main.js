d61a1f2320e025ca3b3abf01853ac866e084a4bd
RickyCT13 <ricardomorenocantea13@gmail.com>
2024-03-04 13:07:39 +0100

    4/3/2024

---
+++ Tema-07/Ejemplos/01/AccesibilidadForm/src/assets/scripts/main.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+function onIniciar() {
+  console.log("Hola Mundo");
+
+  // Script para cambiar la pestaña en el main
+  const botonesTab = document.querySelectorAll(".listaBotones__botonTab");
+  const bloquesTexto = document.querySelectorAll(".contenedorTexto__bloque");
+  //console.log(botonesTab)
+
+  // Recorremos la lista de botones para añadir un eventListener de click
+  botonesTab.forEach((boton, i) => {
+    //console.log(boton)
+
+    // El evento consiste en quitar la clase activo a todos los botones
+    // y acto seguido añadir la clase activo únicamente al que se le ha hecho click
+    boton.addEventListener("click", () => {
+      botonesTab.forEach((botonF, i) => {
+        botonF.classList.remove("activo");
+      });
+      boton.classList.add("activo");
+    });
+  });
+  
+  //contenedorTexto.addEventListener("click", removeActivo);
+}
+
+
+window.onload = onIniciar();
